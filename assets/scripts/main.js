0f37e450abab9bf616a367ee691ace0c0eb388bb
Justin Nguyen <justnguyen1@gmail.com>
2021-11-05 17:44:15 -0700

    part 2 step 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -61,6 +61,15 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 }
 
 /**
